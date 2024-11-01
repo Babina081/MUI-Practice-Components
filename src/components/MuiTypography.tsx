fd51fb7ae43bc195d0ae53f220eeefe35649784d
Babina081 <babitamoo333@gmail.com>
2024-11-01 12:16:56 +0545

    Add: typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,36 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">MuiTypography</Typography>
+      <Typography variant="h2">MuiTypography</Typography>
+      <Typography variant="h3">MuiTypography</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        MuiTypography
+      </Typography>
+      <Typography variant="h5">MuiTypography</Typography>
+      <Typography variant="h6">MuiTypography</Typography>
+      <Typography variant="subtitle1">MuiTypography</Typography>
+      <Typography variant="subtitle2">MuiTypography</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam
+        suscipit totam officia enim molestias voluptatem eligendi! Corrupti
+        pariatur deserunt non ducimus, expedita ex, eveniet fugit commodi
+        mollitia consectetur est maiores sapiente quod id quas natus quae
+        explicabo repellendus odit dolorum! Quae voluptatibus porro maiores vel
+        ducimus sit, reiciendis non.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam
+        suscipit totam officia enim molestias voluptatem eligendi! Corrupti
+        pariatur deserunt non ducimus, expedita ex, eveniet fugit commodi
+        mollitia consectetur est maiores sapiente quod id quas natus quae
+        explicabo repellendus odit dolorum! Quae voluptatibus porro maiores vel
+        ducimus sit, reiciendis non.
+      </Typography>
+      <Typography variant="caption">MuiTypography</Typography>
+    </div>
+  );
+};
