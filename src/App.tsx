9e0d8ba60aff499ed42d0a317ac518b80295b140
Babina081 <babitamoo333@gmail.com>
2024-11-01 12:25:53 +0545

    Add: Root App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MuiAccordion from "./components/MuiAccordion";
+import MuiAlert from "./components/MuiAlert";
+import MuiAutoComplete from "./components/MuiAutoComplete";
+import MuiAvatar from "./components/MuiAvatar";
+import MuiBadge from "./components/MuiBadge";
+import MuiBottomNavigation from "./components/MuiBottomNavigation";
+import MuiBox from "./components/MuiBox";
+import MuiBreadCrumbs from "./components/MuiBreadCrumbs";
+import { MuiButton } from "./components/MuiButton";
+import MuiButtonGroup from "./components/MuiButtonGroup";
+import MuiCard from "./components/MuiCard";
+import MuiCheckbox from "./components/MuiCheckbox";
+import MuiChips from "./components/MuiChips";
+import MuiDialog from "./components/MuiDialog";
+import MuiDrawer from "./components/MuiDrawer";
+import MuiGrid from "./components/MuiGrid";
+import MuiImageList from "./components/MuiImageList";
+import MuiLink from "./components/MuiLink";
+import MuiList from "./components/MuiList";
+import MuiLoadingButton from "./components/MuiLoadingButton";
+import MuiMenu from "./components/MuiMenu";
+import MuiNav from "./components/MuiNav";
+import MuiPaper from "./components/MuiPaper";
+import MuiProgress from "./components/MuiProgress";
+import MuiRadioButton from "./components/MuiRadioButton";
+import MuiRating from "./components/MuiRating";
+import MuiSelect from "./components/MuiSelect";
+import MuiSkeleton from "./components/MuiSkeleton";
+import MuiSnackbar from "./components/MuiSnackbar";
+import MuiSpeedDialComp from "./components/MuiSpeedDialComp";
+import MuiStack from "./components/MuiStack";
+import MuiSwitch from "./components/MuiSwitch";
+import MuiTable from "./components/MuiTable";
+import MuiTextField from "./components/MuiTextField";
+import MuiToggleButton from "./components/MuiToggleButton";
+import MuiToolTip from "./components/MuiToolTip";
+import { MuiTypography } from "./components/MuiTypography";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <MuiTypography></MuiTypography> */}
+      {/* <MuiButton></MuiButton> */}
+      {/* <MuiButtonGroup></MuiButtonGroup> */}
+      {/* <MuiToggleButton></MuiToggleButton> */}
+      {/* <MuiTextField></MuiTextField> */}
+      {/* <MuiSelect></MuiSelect> */}
+      {/* <MuiRadioButton></MuiRadioButton> */}
+      {/* <MuiCheckbox></MuiCheckbox> */}
+      {/* <MuiSwitch></MuiSwitch> */}
+      {/* <MuiRating></MuiRating> */}
+      {/* <MuiAutoComplete></MuiAutoComplete> */}
+      {/* <MuiBox></MuiBox> */}
+      {/* <MuiStack></MuiStack> */}
+      {/* <MuiGrid></MuiGrid> */}
+      {/* <MuiPaper></MuiPaper> */}
+      {/* <MuiCard></MuiCard> */}
+      {/* <MuiAccordion></MuiAccordion> */}
+      {/* <MuiImageList></MuiImageList> */}
+      {/* <MuiNav></MuiNav> */}
+      {/* <MuiMenu></MuiMenu> */}
+      {/* <MuiLink></MuiLink> */}
+      {/* <MuiBreadCrumbs></MuiBreadCrumbs> */}
+      {/* <MuiDrawer></MuiDrawer> */}
+      {/* <MuiSpeedDialComp></MuiSpeedDialComp> */}
+      {/* <MuiBottomNavigation></MuiBottomNavigation> */}
+      {/* <MuiAvatar></MuiAvatar> */}
+      {/* <MuiBadge></MuiBadge> */}
+      {/* <MuiList></MuiList> */}
+      {/* <MuiChips></MuiChips> */}
+      {/* <MuiToolTip></MuiToolTip> */}
+      {/* <MuiTable></MuiTable> */}
+      {/* <MuiAlert></MuiAlert> */}
+      {/* <MuiSnackbar></MuiSnackbar> */}
+      {/* <MuiDialog></MuiDialog> */}
+      {/* <MuiProgress></MuiProgress> */}
+      {/* <MuiSkeleton></MuiSkeleton> */}
+      <MuiLoadingButton></MuiLoadingButton>
     </div>
   );
 }
